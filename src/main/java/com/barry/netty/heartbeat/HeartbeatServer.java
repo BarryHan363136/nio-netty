0f51cef2e1fee245187bcf4d379d9cb10e358401
QXV0963 <Tongshan.Han @partner. bmw.com>
2019-12-31 16:09:12 +0800

    netty heartbeat: server & client

---
+++ src/main/java/com/barry/netty/heartbeat/HeartbeatServer.java
@@ -0,0 +1,52 @@
+package com.barry.netty.heartbeat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+import java.util.concurrent.TimeUnit;
+
+public class HeartbeatServer {
+
+    public static void main(String[] args) {
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast("decoder", new StringDecoder());
+                            pipeline.addLast("encoder", new StringEncoder());
+                            //IdleStateHandler的readIdleTime参数指定超过3秒还没收到客户端的连接
+                            //会触发IdleStateEvent事件并且交给下一个handler处理, 下一个handler必须
+                            //实现userEventTriggered方法处理对应事件
+                            pipeline.addLast(new IdleStateHandler(3, 0, 0, TimeUnit.SECONDS));
+                            pipeline.addLast(new HeartBeatServerHandler());
+                        }
+                    });
+            System.out.println("NETTY SERVER START。。。");
+            ChannelFuture future = bootstrap.bind(8085).sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            worker.shutdownGracefully();
+            boss.shutdownGracefully();
+        }
+
+
+    }
+
+
+}
